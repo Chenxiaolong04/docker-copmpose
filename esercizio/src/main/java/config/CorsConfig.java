9013b77ceb29ca0298c4a32c9535b7f848e787d2
Chenxiaolong04 <chenxiaolong12345678@gmail.com>
2025-05-27 10:10:46 +0200

    Add files via upload

---
+++ esercizio/src/main/java/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package config; // usa il package giusto se serve
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // <-- questo è fondamentale
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
